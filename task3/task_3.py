157f0ffcdf42d34bbfbefea6b653cd05613aec24
kiririri <kirillsaitov44@gmail.com>
2024-11-18 20:19:25 +0500

    complete task2

---
+++ task3/task_3.py
@@ -0,0 +1,20 @@
+def print_valid_cities(all_cities, used_cities):
+   cities_different = all_cities.difference(used_cities)
+   for city in cities_different:
+        print(city)
+
+all_cities = {
+    'Абакан',
+    'Астрахань',
+    'Бобруйск',
+    'Калуга',
+    'Караганда',
+    'Кострома',
+    'Липецк',
+    'Новосибирск'
+}
+
+used_cities = {'Калуга', 'Абакан' , 'Новосибирск'}
+
+print_valid_cities(all_cities, used_cities)
+#hhhhh
