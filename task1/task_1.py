dbf2364c7b516129c3dafd69150f35dc1ade3bff
kiririri <kirillsaitov44@gmail.com>
2024-11-21 00:11:02 +0500

    Rewrote the if block using ternary operators

--- task1/task_1.py
+++ task1/task_1.py
@@ -2,16 +2,15 @@ milk = False  # Молока нет.
 cereal = True # Хлопья есть.
 eggs = False  # Яиц нет.
 
-breakfast = ("- омлет"
-             if not eggs and (not milk or cereal)
-             else "- яичница"
-             if not milk else
-             "- хлопья с молоком"
-             if cereal else
-             ("- стакан молока"
-              if milk else
-              "можно погрызть сухих хлопьев"
-              if cereal else
-              "ничего не будет: разгрузочный день"))
+breakfast = (
+    "- омлет" if not eggs and (not milk or cereal) else
+    "- яичница" if not milk else
+    "- хлопья с молоком" if cereal else
+    (
+        "- стакан молока" if milk else
+        "можно погрызть сухих хлопьев" if cereal else
+        "ничего не будет: разгрузочный день"
+    )
+)
 
 print("Сегодня на завтрак", breakfast)
