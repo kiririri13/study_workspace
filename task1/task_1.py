c11d917309784548eb73cbb263a3700d607f3ff4
kiririri <kirillsaitov44@gmail.com>
2024-11-20 13:44:14 +0500

    Rewrote the if block using ternary operators

--- task1/task_1.py
+++ task1/task_1.py
@@ -1,23 +1,17 @@
-# Продуктов маловато:
-milk = False          # Молока нет.
-cereal = True         # Хлопья есть.
-eggs = False          # Яиц нет.
+milk = False  # Молока нет.
+cereal = True # Хлопья есть.
+eggs = False  # Яиц нет.
 
-# Вставьте логический оператор вместо многоточия.
+breakfast = ("- омлет"
+             if not eggs and (not milk or cereal)
+             else "- яичница"
+             if not milk else
+             "- хлопья с молоком"
+             if cereal else
+             ("- стакан молока"
+              if milk else
+              "можно погрызть сухих хлопьев"
+              if cereal else
+              "ничего не будет: разгрузочный день"))
 
-if milk and cereal or eggs:
-    if not eggs:
-        if not milk:
-            breakfast = "- омлет"
-        else:
-            breakfast = "- яичница"
-    else:
-        breakfast = "- хлопья с молоком"
-else:
-    if milk:
-        breakfast = "- стакан молока"
-    elif cereal:
-        breakfast = "можно погрызть сухих хлопьев"
-    else:
-        breakfast = "ничего не будет: разгрузочный день"
 print("Сегодня на завтрак", breakfast)
