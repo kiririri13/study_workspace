1559a50c522f057cc724e291d63e795f91cf65fb
kiririri <kirillsaitov44@gmail.com>
2024-11-26 17:56:10 +0500

    complete Task 8

---
+++ task8/task_8.py
@@ -0,0 +1,60 @@
+mobile_devices = {
+    'cucuPhone': 2010,
+    'cucuBlet': 2013,
+    'cucuClock': 2015,
+    'cucuEar': 2018,
+    'cuCube': 2015,
+}
+
+home_devices = {
+    'cucuLot': 2011,
+    'cucuBlock': 2010,
+    'cucuWall': 2010,
+    'cucuMonitor': 2020,
+    'cucuLamp': 2015,
+    'cucuTable': 2016,
+    'cucuTV': 2017,
+}
+
+not_supported_devices = {'cucuBlock', 'cucuBlet', 'cucuWall'}
+
+result_catalog = {}
+
+# Переменная для хранения поддерживаемых устройств (множество)
+supported_devices = set()
+
+
+# Функция для выборки поддерживаемого девайса из словаря
+def get_supported_catalog(dict_devices, device):
+    # Если устройство присутствует в словаре и не находится в списке не поддерживаемых
+    if device in dict_devices and device not in not_supported_devices:
+        # Возвращаем словарь с парой <device>: <год выпуска>
+        return {device: dict_devices[device]}
+    return {}
+
+
+# Переменная для объединения всех устройств (множество)
+all_devices = set(mobile_devices.keys()).union(home_devices.keys())
+
+# Процесс добавления поддерживаемых устройств в множество
+for device in all_devices:
+    # Проверяем и добавляем мобильные устройства
+    supported_mob_dev = get_supported_catalog(mobile_devices, device)
+    if supported_mob_dev:
+        supported_devices.update(supported_mob_dev.keys())  # Добавляем только ключи (названия устройств)
+
+    # Проверяем и добавляем домашние устройства
+    supported_home_dev = get_supported_catalog(home_devices, device)
+    if supported_home_dev:
+        supported_devices.update(supported_home_dev.keys())  # Добавляем только ключи (названия устройств)
+
+# Заполняем итоговый каталог с помощью поддерживаемых устройств и их года выпуска
+for device in supported_devices:
+    if device in mobile_devices:
+        result_catalog[device] = mobile_devices[device]
+    elif device in home_devices:
+        result_catalog[device] = home_devices[device]
+
+# Вывод результата
+print('Каталог поддерживаемых девайсов:')
+print(result_catalog)
